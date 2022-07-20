154bc18cc6d97454ad2eed44233ec76e06e956cc
Trish Fox-Collis <tfoxcollis@gmail.com>
2022-07-20 08:39:50 -0700

    successful fetch in component did mount

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import '../css/App.css';
 
 class App extends Component {
+    constructor(){
+      super()
+      this.state = {
+        reservations: []
+      }
+    }
+
+componentDidMount(){
+  fetch("http://localhost:3001/api/v1/reservations")
+  .then(response => response.json())
+  .then(data => this.setState({reservations: data}))
+}
+
   render() {
     return (
       <div className="App">
